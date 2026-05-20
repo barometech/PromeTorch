4861d006a5178f27fa44ce2db1b0a5c76a70eefd
barometech <barometech@gmail.com>
2026-05-20 11:15:10 +0300

    revert(repo): убрать локальные debug-files которые случайно попали в ea0292d
    git add -A в прошлом commit'е захватил untracked локальные файлы:
      - nm_quad_qwen/*.{py,c}  (16 debug scripts для NM Quad Qwen dumping)
      - tests/io/test_pt8_loader.cpp
    
    Это локальные artifacts, не для public repo. Untracked через git rm
    --cached + добавлены в .gitignore чтоб больше не попадали при
    'git add -A'.
    
    Файлы СОХРАНЕНЫ на диске (только убраны из git tracking).

--- nm_quad_qwen/nmc_noop.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "nm6408load_nmc.h"
-int main(int argc, char *argv[]) {
-    (void)argc; (void)argv;
-    printf("NMC%d:%d noop\n", ncl_getClusterID(), ncl_getCoreID());
-    return 0;
-}
